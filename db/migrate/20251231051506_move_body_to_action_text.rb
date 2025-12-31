d601021ca980be7494ad4066f7459e201c5eca48
benfminer <benfminer@gmail.com>
2025-12-30 22:15:16 -0800

    first commit

---
+++ db/migrate/20251231051506_move_body_to_action_text.rb
@@ -0,0 +1,7 @@
+class MoveBodyToActionText < ActiveRecord::Migration[8.1]
+  def change
+    BlogPost.all.find_each do |blog_post|
+      blog_post.update(content: blog_post.body)
+    end
+  end
+end
